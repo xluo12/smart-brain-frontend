283cf762de51d1c5fff4ba5fcb1678f074a0764f
Xiaoyu Luo <xluoforhire@gmail.com>
2019-04-07 13:56:16 -0400

    add unique key to each box in box array

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -8,7 +8,7 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
         <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
         {
           boxes.map(box => {
-            return <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            return <div key={box.topRow}className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
           })
         }      
       </div>
